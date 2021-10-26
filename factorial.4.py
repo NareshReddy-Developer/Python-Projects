b64e36727b6db3cde5245d3230d0930f6ad74a75
NareshReddy <89729667+NareshReddy-Developer@users.noreply.github.com>
2021-10-27 03:05:13 +0530

    Add files via upload

---
+++ factorial.4.py
@@ -0,0 +1,20 @@
+# Factorial
+import math
+print("The factorial of 10 is :",end="")
+print(math.factorial(10))
+# Calender
+
+import calendar
+print(calendar.calendar(2021))
+
+#
+import calendar
+print(calendar.isleap(2012))
+
+import math
+x=10
+y=12
+z=14
+print("The factorial of 10 is :",math.factorial(x))
+print("The factorial of 12 is :",math.factorial(y))
+print("The factorial of 14 is :",math.factorial(z))
